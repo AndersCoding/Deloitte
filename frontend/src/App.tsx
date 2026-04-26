e8bd4e24ccf6debb345c3de4c862b88ae34bba64
AndersB <97296236+AndersCoding@users.noreply.github.com>
2026-04-26 18:27:49 +0200

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,35 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import InfoBox from "./Components/InfoBox";
+import axios from "axios";
+import SearchMovie from "./Components/SearchMovie";
 
+interface Movie {
+  title: string;
+  episode_id: number;
+}
 function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  // Fetch movies
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get("https://swapi.info/api/films/");
+        setMovies(response.data);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+    fetchMovies();
+  }, []);
 
   return (
     <>
-        <InfoBox />
+      <InfoBox />
       <section id="center">
         <h3>Deloitte Account</h3>
+        <SearchMovie movies={movies} />
       </section>
     </>
   );
