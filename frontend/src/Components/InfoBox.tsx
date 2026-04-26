1532f0b25ad950ae6b51144ff91111cb6ba76d63
AndersB <97296236+AndersCoding@users.noreply.github.com>
2026-04-26 17:28:51 +0200

    added axios

---
+++ frontend/src/Components/InfoBox.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function InfoBox() {
+  return (
+    <div className="info-box">
+      <h3>
+        Legg til konto med saldo, og få foreslått konto å plassere beløpet på
+      </h3>
+    </div>
+  );
+}
