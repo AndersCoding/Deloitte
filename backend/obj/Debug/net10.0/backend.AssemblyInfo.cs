48d322a4f39bc398a8e539d47caad48a0f01b1ef
AndersB <97296236+AndersCoding@users.noreply.github.com>
2026-04-27 23:29:19 +0200

    updated

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4872a5beecce6051c449348dee14abd3ca78f6cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6e08a1fcc95cbc2a61031d022a8b4a6e37ed40a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
